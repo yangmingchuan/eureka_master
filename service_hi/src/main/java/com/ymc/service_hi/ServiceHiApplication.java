0ea977867e96fb49bc9323410c5297b87a5466f1
yangmingchuan <18768880074@163.com>
2019-06-17 10:33:25 +0800

    服务链接情况

--- service_hi/src/main/java/com/ymc/service_hi/ServiceHiApplication.java
+++ service_hi/src/main/java/com/ymc/service_hi/ServiceHiApplication.java
@@ -1,13 +1,41 @@
 package com.ymc.service_hi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class ServiceHiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceHiApplication.class, args);
 	}
 
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+	@RequestMapping("/hi")
+	public String callHome(){
+		return restTemplate.getForObject("http://localhost:9017/miya", String.class);
+	}
+	@RequestMapping("/info")
+	public String info(){
+		return "i'm service-hi";
+	}
+
+	@Bean
+	public AlwaysSampler defaultSampler(){
+		return new AlwaysSampler();
+	}
 }
--- service_miya/src/main/java/com/ymc/service_miya/ServiceMiyaApplication.java
+++ service_miya/src/main/java/com/ymc/service_miya/ServiceMiyaApplication.java
@@ -1,13 +1,37 @@
 package com.ymc.service_miya;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class ServiceMiyaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceMiyaApplication.class, args);
+	}
+
+	@RequestMapping("/hi")
+	public String home(){
+		return "hi i'm miya!";
+	}
+
+	@RequestMapping("/miya")
+	public String info(){
+		return restTemplate.getForObject("http://localhost:9016/info",String.class);
+	}
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
 	}
 
 }
